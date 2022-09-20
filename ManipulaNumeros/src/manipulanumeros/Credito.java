c53fe52c565f779645afcb7ca099494d3ed0a2a7
Joao Nogueira <jnprogcencal@gmail.com>
2022-09-20 16:41:41 +0100

    Calcular credito melhorado

--- ManipulaNumeros/src/manipulanumeros/Credito.java
+++ ManipulaNumeros/src/manipulanumeros/Credito.java
@@ -5,22 +5,24 @@ package manipulanumeros;
 class Credito {
 
     static void calculaCredito() {
-        int saldoMedio;
-        System.out.println("Insira saldo médio anual.");
-        saldoMedio = ManipulaNumeros.ler.nextInt();
-        System.out.println(saldoMedio/201);
+        int saldoMedio, saldoCase;
+            System.out.println("Insira saldo médio anual.");
+            saldoMedio = ManipulaNumeros.ler.nextInt();
+        saldoCase = saldoMedio;
+        if (saldoMedio <0)
+            saldoMedio = 0;
         switch (saldoMedio/201){
             case 0:
-                System.out.println("\nSaldo médio "+saldoMedio+"\nNão tem direito a crédito\n");
+                System.out.println("\nSaldo médio "+saldoCase+"\nNão tem direito a crédito\n");
                 break;
             case 1:
-                System.out.println("\nSaldo médio "+saldoMedio+"\nTem crédito de "+(saldoMedio*0.2)+"\n");
+                System.out.println("\nSaldo médio "+saldoCase+"\nTem crédito de "+(saldoMedio*0.2)+"\n");
                 break;
             case 2:
-                System.out.println("\nSaldo médio "+saldoMedio+"\nTem crédito de "+(saldoMedio*0.3)+"\n");
+                System.out.println("\nSaldo médio "+saldoCase+"\nTem crédito de "+(saldoMedio*0.3)+"\n");
                 break;
             default: 
-                System.out.println("\nSaldo médio "+saldoMedio+"\nTem crédito de "+(saldoMedio*0.4)+"\n"); 
+                System.out.println("\nSaldo médio "+saldoCase+"\nTem crédito de "+(saldoMedio*0.4)+"\n"); 
             }
     }
     
